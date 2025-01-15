bda566af2191d05ff031bd262eb8ac6c59f70ef9
Eryk Dziedzic <eryk.marian.dziedzic@gmail.com>
2025-01-15 18:57:12 +0100

    feat: add notarization support for macOS and update environment configuration

---
+++ notarize.js
@@ -0,0 +1,19 @@
+require('electron-notarize').notarize;
+
+exports.default = async function notarize(context) {
+  const { electronPlatformName, appOutDir } = context;
+  if (electronPlatformName !== 'darwin') {
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+
+  console.log('Notarizing', appName);
+
+  return await notarize({
+    appBundleId: 'frolickers-video-processing',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLE_ID,
+    appleIdPassword: process.env.APPLE_ID_PASSWORD,
+  });
+};
